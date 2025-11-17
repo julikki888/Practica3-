10b7e6bf79e3a2112bf60b13904d7240db8622ff
1DAW <1DAW@DESKTOP-84KDH6P>
2025-11-17 14:37:55 +0100

    ed

--- practica3/src/practica3/TestLibreria1.java
+++ practica3/src/practica3/TestLibreria1.java
@@ -1,5 +1,6 @@
 package practica3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TestLibreria1 {
@@ -20,15 +21,15 @@ public class TestLibreria1 {
 		 * 7. Calcular la función de Euler de un número. 
 		 * • Salir
 		 */
-		int menu;
-		boolean sw = true;
+		int menu=0;
 		try (Scanner sc = new Scanner(System.in)) {
 			
-		while (sw) {
-			try(sc){
+		do {
+			try{
 				
 				Menu();//muestra las opciones
 				menu = sc.nextInt();
+				
 				switch (menu) {
 				case 1 -> CalcularFactorial(sc);
 				case 2 -> CalcularCombinatorio(sc);
@@ -37,15 +38,15 @@ public class TestLibreria1 {
 				case 5 -> AmigosComprendidos(sc);
 				case 6 -> SonPrimosEntreSi(sc);
 				case 7 -> CalculaEuler(sc);
-				case 8 -> sw=false;
+				case 8 -> System.out.println("Saliendo....");
 				default ->
 				throw new IllegalArgumentException("Unexpected value: " + menu);
 				}
+			}catch (InputMismatchException e) {
 				sc.nextLine();
-			}catch (Exception e) {
-					// TODO: handle exception
+				System.out.println("Debe teclear un valor numerico");
 				}
-			}
+			}while (menu!=8);
 		System.out.println("\nFIN DEL PROGRAMA");
 		}
 
@@ -66,8 +67,8 @@ public class TestLibreria1 {
 	}
 	
 	
-	public static void CalcularFactorial(Scanner sc) {
-		int num, fact;
+	public static void CalcularFactorial(Scanner sc) {//Opcion 1. Se introduce un numero y se calcula 
+		int num, fact;								  		//	   su factorial usando el metodo de la libreria.
 		System.out.println("Introduce el numero a factorizar: ");
 		num = sc.nextInt();			
 		fact=MiLibreria.Factorial(num);
@@ -77,21 +78,23 @@ public class TestLibreria1 {
 	}
 	
 	
-	public static void CalcularCombinatorio(Scanner sc) {
+	public static void CalcularCombinatorio(Scanner sc) {//Opcion 2. Se Introducen dos numeros y se calcula
 		int num1,num2;
+		double combi=1;										// 		  el combinatorio usando un metodo de mi libreria
 		System.out.println("Introduce el primer numero combinatorio: ");
 		num1 = sc.nextInt();	
 		System.out.println("Introduce el segundo numero combinatorio: ");
 		num2 = sc.nextInt();
-		
-		System.out.println("El calculo de combinatorios es: "+MiLibreria.Combinatorio(num1, num2));
+		combi=MiLibreria.Combinatorio(num1, num2);
+		System.out.println("El calculo de combinatorios es: "+(combi<0?"Numeros introducidos erroneos.":combi));
 		
 	}
 	
 	
-	public static void PrimosComprendidos(Scanner sc) {
-		int num1,num2,aux;
-		System.out.println("Introduce el primer numero: ");
+	public static void PrimosComprendidos(Scanner sc) {//Opcion 3. Se introducen dos numeros donde ordenaremos poniendo el mayor en num1 
+		int num1,num2,aux;										//  y el menor en num2. Luego con un bucle for recoremos los numeros comprendidos  
+		boolean sw=true;										//  entre ambos comprobando si son primos. Y avisando si no hay
+		System.out.println("Introduce el primer numero: ");		
 		num1 = sc.nextInt();	
 		System.out.println("Introduce el segundo numero: ");
 		num2 = sc.nextInt();
@@ -103,13 +106,18 @@ public class TestLibreria1 {
 		}
 		System.out.println("Primos comprendidos entre "+num1+" y "+num2+":\n");
 		for(int i=num2+1;i<num1;i++) {
-			if(MiLibreria.EsPrimo(i))System.out.println(i+"\t");	
+			if(MiLibreria.EsPrimo(i)) {
+				System.out.println(i+"\t");	
+				sw=false;
+			}
 		}
+		if(sw)System.out.println("No hay primos entre estos numeros.");
 	}
 	
 	
-	public static void PerfectosComprendidos(Scanner sc) {
-		int num1,num2,aux;
+	public static void PerfectosComprendidos(Scanner sc) {//Opcion 4. Se introducen dos numeros donde ordenaremos poniendo el mayor en num1 
+		int num1,num2,aux;											//  y el menor en num2. Luego con un bucle for recoremos los numeros comprendidos 
+		boolean sw=true;											//  entre ambos comprobando si son perfectos. Y avisando si no hay
 		System.out.println("Introduce el primer numero: ");
 		num1 = sc.nextInt();	
 		System.out.println("Introduce el segundo numero: ");
@@ -122,13 +130,19 @@ public class TestLibreria1 {
 		}
 		System.out.println("Perfectos comprendidos entre "+num1+" y "+num2+":\n");
 		for(int i=num2+1;i<num1;i++) {
-			if(MiLibreria.Perfecto(i))System.out.println(i+"\t");	
+			if(MiLibreria.Perfecto(i)) {
+				sw=false;
+				System.out.println(i+"\t");	
+			}
 		}
+		if(sw)System.out.println("No hay perfectos comprendidos entre estos numeros.");
+
 	}
 	
 	
-	public static void AmigosComprendidos(Scanner sc) {
-		int num1,num2,aux;
+	public static void AmigosComprendidos(Scanner sc) {//Opcion 5. Se introducen dos numeros donde ordenaremos poniendo el mayor en num1 
+		int num1,num2,aux;										//  y el menor en num2. Luego con un bucle for recoremos los numeros comprendidos 
+		boolean sw=true;										//  entre ambos comprobando si son Amigos. Y avisando si no hay
 		System.out.println("Introduce el primer numero: ");
 		num1 = sc.nextInt();	
 		System.out.println("Introduce el segundo numero: ");
@@ -142,12 +156,16 @@ public class TestLibreria1 {
 		System.out.println("Amigos comprendidos entre "+num1+" y "+num2+":\n");
 		for(int i=num2+1;i<num1;i++) {
 			for(int j=num2+1;j<num1;j++)
-			if(MiLibreria.Amigos(i,j))System.out.println("Son amigos: "+i+"  "+j);	
+			if(MiLibreria.Amigos(i,j)) {
+				sw=false;
+				System.out.println("Son amigos: "+i+"  "+j);	
+			}
 		}
+		if(sw)System.out.println("No hay amigos comprendidos entre estos numeros.");
 	}
 	
-	public static void SonPrimosEntreSi(Scanner sc) {
-		int num1,num2;
+	public static void SonPrimosEntreSi(Scanner sc) {//Opcion 6. Se introducen dos numeros y usando un metodo de la libreria
+		int num1,num2;											// comprobamos si son primos entre si.
 		System.out.println("Introduce el primer numero: ");
 		num1 = sc.nextInt();	
 		System.out.println("Introduce el segundo numero: ");
@@ -157,7 +175,7 @@ public class TestLibreria1 {
 		else System.out.println("No son primos entre si ;(");
 	}
 	
-	public static void CalculaEuler(Scanner sc) {
+	public static void CalculaEuler(Scanner sc) {//Opcion 7. Se introduce un numero y usando un metodo de la libreria se calcula el euler
 		int num;
 		System.out.println("Introduce el numero a calcular Euler: ");
 		num = sc.nextInt();	
